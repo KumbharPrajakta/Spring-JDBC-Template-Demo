4dca47d6c60d2e8e04ed9b9920116b373752abff
KumbharPrajakta <kumbharprajakta105@gmail.com>
2023-01-03 17:36:00 +0530

    update

--- jdbctemplatedemo/src/main/java/com/csi/model/Employee.java
+++ jdbctemplatedemo/src/main/java/com/csi/model/Employee.java
@@ -1,4 +1,34 @@
 package com.csi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Employee {
+
+    @Id
+    private int empId;
+
+    private String empName;
+
+    private String empAddress;
+
+    private long empContact;
+
+    private Date empDOB;
+
+    private double empSalary;
+
+    private String empEmailid;
+
+    private String empPassword;
+
 }
--- jdbctemplatedemo/src/main/java/com/csi/service/EmployeeService.java
+++ jdbctemplatedemo/src/main/java/com/csi/service/EmployeeService.java
@@ -1,4 +1,48 @@
 package com.csi.service;
 
+import com.csi.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    public Employee signUp(Employee employee);
+
+    public boolean signIn(String empEmailId, String empPassword);
+
+    public List<Employee> getAllData();
+
+    public Employee update(int empId,Employee employee);
+
+    public Employee getDataById(int empId);
+
+    public List<Employee> getDataByName(String empName);
+
+    public Employee getDataByEmailId(String empEmailId);
+
+    public Employee getDataByContact(long empContact);
+
+    public List<Employee> getDataByUsingAnyInput(String input);
+
+    public List<Employee> sortById();
+
+    public List<Employee> sortByName();
+
+    public List<Employee> sortEmailId();
+
+    public List<Employee> sortByContact();
+
+    public List<Employee> sortBySalary();
+
+    public List<Employee> sortByDOB();
+
+    public List<Employee> filterBySalary(double empSalary);
+
+    public boolean isEligibleForLoan(double empSalary);
+
+    public Employee fetchSecondLargestSalary();
+
+    public void deleteById(int empId);
+
+    public void deleteAll();
 }
